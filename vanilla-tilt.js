4973edc4b828a405fd9d27a41668f60eb5e72f5f
Sivanithipy <pyshivanithi@gmail.com>
2024-09-30 13:36:17 +0530

    made some changes

--- vanilla-tilt.js
+++ vanilla-tilt.js
@@ -340,6 +340,163 @@ var VanillaTilt = (function () {
         "pointer-events": "none",
         "border-radius": "inherit"
       });
+      Object.assign(this.glareElement.style, {
+        "position": "absolute",
+        "top": "50%",
+        "left": "50%",
+        "pointer-events": "none",
+        "background-image": `linear-gradient(0deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%)`,
+        "transform": "rotate(180deg) translate(-50%, -50%)",
+        "transform-origin": "0% 0%",
+        "opacity": "0"
+      });
   
+      this.updateGlareSize();
+    }
+  
+    updateGlareSize() {
+      if (this.glare) {
+        const glareSize = (this.element.offsetWidth > this.element.offsetHeight ? this.element.offsetWidth : this.element.offsetHeight) * 2;
+  
+        Object.assign(this.glareElement.style, {
+          "width": `${glareSize}px`,
+          "height": `${glareSize}px`,
+        });
+      }
+    }
+  
+    updateClientSize() {
+      this.clientWidth = window.innerWidth
+        || document.documentElement.clientWidth
+        || document.body.clientWidth;
+  
+      this.clientHeight = window.innerHeight
+        || document.documentElement.clientHeight
+        || document.body.clientHeight;
+    }
+  
+    onWindowResize() {
+      this.updateGlareSize();
+      this.updateClientSize();
+    }
+  
+    setTransition() {
+      clearTimeout(this.transitionTimeout);
+      this.element.style.transition = this.settings.speed + "ms " + this.settings.easing;
+      if (this.glare) this.glareElement.style.transition = `opacity ${this.settings.speed}ms ${this.settings.easing}`;
+  
+      this.transitionTimeout = setTimeout(() => {
+        this.element.style.transition = "";
+        if (this.glare) {
+          this.glareElement.style.transition = "";
+        }
+      }, this.settings.speed);
+  
+    }
+  
+    /**
+     * Method return patched settings of instance
+     * @param {boolean} settings.reverse - reverse the tilt direction
+     * @param {number} settings.max - max tilt rotation (degrees)
+     * @param {startX} settings.startX - the starting tilt on the X axis, in degrees. Default: 0
+     * @param {startY} settings.startY - the starting tilt on the Y axis, in degrees. Default: 0
+     * @param {number} settings.perspective - Transform perspective, the lower the more extreme the tilt gets
+     * @param {string} settings.easing - Easing on enter/exit
+     * @param {number} settings.scale - 2 = 200%, 1.5 = 150%, etc..
+     * @param {number} settings.speed - Speed of the enter/exit transition
+     * @param {boolean} settings.transition - Set a transition on enter/exit
+     * @param {string|null} settings.axis - What axis should be enabled. Can be "x" or "y"
+     * @param {boolean} settings.glare - if it should have a "glare" effect
+     * @param {number} settings.max-glare - the maximum "glare" opacity (1 = 100%, 0.5 = 50%)
+     * @param {boolean} settings.glare-prerender - false = VanillaTilt creates the glare elements for you, otherwise
+     * @param {boolean} settings.full-page-listening - If true, parallax effect will listen to mouse move events on the whole document, not only the selected element
+     * @param {string|object} settings.mouse-event-element - String selector or link to HTML-element what will be listen mouse events
+     * @param {boolean} settings.reset - false = If the tilt effect has to be reset on exit
+     * @param {boolean} settings.reset-to-start - true = On reset event (mouse leave) will return to initial start angle (if startX or startY is set)
+     * @param {gyroscope} settings.gyroscope - Enable tilting by deviceorientation events
+     * @param {gyroscopeSensitivity} settings.gyroscopeSensitivity - Between 0 and 1 - The angle at which max tilt position is reached. 1 = 90deg, 0.5 = 45deg, etc..
+     * @param {gyroscopeSamples} settings.gyroscopeSamples - How many gyroscope moves to decide the starting position.
+     */
+    extendSettings(settings) {
+      let defaultSettings = {
+        reverse: false,
+        max: 15,
+        startX: 0,
+        startY: 0,
+        perspective: 1000,
+        easing: "cubic-bezier(.03,.98,.52,.99)",
+        scale: 1,
+        speed: 300,
+        transition: true,
+        axis: null,
+        glare: false,
+        "max-glare": 1,
+        "glare-prerender": false,
+        "full-page-listening": false,
+        "mouse-event-element": null,
+        reset: true,
+        "reset-to-start": true,
+        gyroscope: true,
+        gyroscopeMinAngleX: -45,
+        gyroscopeMaxAngleX: 45,
+        gyroscopeMinAngleY: -45,
+        gyroscopeMaxAngleY: 45,
+        gyroscopeSamples: 10
+      };
+  
+      let newSettings = {};
+      for (var property in defaultSettings) {
+        if (property in settings) {
+          newSettings[property] = settings[property];
+        } else if (this.element.hasAttribute("data-tilt-" + property)) {
+          let attribute = this.element.getAttribute("data-tilt-" + property);
+          try {
+            newSettings[property] = JSON.parse(attribute);
+          } catch (e) {
+            newSettings[property] = attribute;
+          }
+  
+        } else {
+          newSettings[property] = defaultSettings[property];
+        }
+      }
+  
+      return newSettings;
+    }
+  
+    static init(elements, settings) {
+      if (elements instanceof Node) {
+        elements = [elements];
+      }
+  
+      if (elements instanceof NodeList) {
+        elements = [].slice.call(elements);
+      }
+  
+      if (!(elements instanceof Array)) {
+        return;
+      }
+  
+      elements.forEach((element) => {
+        if (!("vanillaTilt" in element)) {
+          element.vanillaTilt = new VanillaTilt(element, settings);
+        }
+      });
+    }
+  }
+  
+  if (typeof document !== "undefined") {
+    /* expose the class to window */
+    window.VanillaTilt = VanillaTilt;
+  
+    /**
+     * Auto load
+     */
+    VanillaTilt.init(document.querySelectorAll("[data-tilt]"));
+  }
+  
+  return VanillaTilt;
+  
+  }());
  
   
